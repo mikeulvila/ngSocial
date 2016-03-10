d28737ef1afa885259ea800c55ba0d36f85fc988
Mike Ulvila <mikeulvila@me.com>
2016-03-10 08:30:59 -0600

    getting users posts

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -11,7 +11,7 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
 
 .config(function( $facebookProvider ) {
   $facebookProvider.setAppId('840567996070980');
-  $facebookProvider.setPermissions('email', 'public_profile', 'user_posts', 'publish_actions', 'user_photos');
+  $facebookProvider.setPermissions('email,public_profile,user_posts,publish_actions,user_photos');
 })
 
 .run(function ($rootScope) {
@@ -25,10 +25,47 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
 })
 
 .controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+
   $scope.isLoggedIn = false;
+
   $scope.login = function () {
     $facebook.login().then(function(){
+      $scope.isLoggedIn = true;
+      refresh();
       console.log('Logged in!')
     });
+  };
+
+  $scope.logout = function () {
+    $facebook.logout().then(function(){
+      $scope.isLoggedIn = false;
+      refresh();
+      console.log('Logged out!')
+    });
+  };
+
+  function refresh () {
+    $facebook.api('/me', {fields: 'id,name,last_name,first_name,email,gender,locale,link'}).then(function (response) {
+      console.log('response>>>', response)
+      $scope.isLoggedIn = true;
+      $scope.welcomeMsg = 'Welcome ' + response.name;
+      $scope.userInfo = response;
+      $facebook.api('/me/picture').then(function (picture) {
+        $scope.picture = picture.data.url;
+        $facebook.api('me/permissions').then(function (permissions) {
+          $scope.permissions = permissions.data;
+          $facebook.api('/me/posts').then(function (posts) {
+            console.log('posts>>>', posts)
+            $scope.posts = posts.data;
+          });
+        });
+      });
+    }, function (err) {
+      $scope.welcomeMsg = 'Please Log In';
+    });
   }
+
+  refresh();
+
+
 }]);
